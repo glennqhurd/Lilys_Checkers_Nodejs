e18da7aecb07b23ab97ee50d29a142c093a6bd84
Glenn Hurd <glennqhurd@gmail.com>
2017-10-11 21:48:07 -0400

    More work learning modules.

--- checkers_database.js
+++ checkers_database.js
@@ -1,4 +1,7 @@
 var user_data = ['Glenn', 'Lyman', 'Lily', 'Delaney'];
+var game_numbers = [];
+var current_game_number = 1;
+var games_in_progress = {};
 
 exports.get_user_data = function() {
     return user_data;
@@ -18,4 +21,28 @@ exports.remove_user_data = function(removed_name) {
     else {
         console.log('Name is not in array');
     }
+};
+
+exports.start_new_game = function(player1, player2) {
+    if((player1 !== player2) && user_data.includes(player1) && user_data.includes(player2)) {
+        game_numbers.push(current_game_number);
+        games_in_progress[current_game_number] = [player1, player2, 'b:bbbbbbbbbbbb--------rrrrrrrrrrrr'];
+        current_game_number++;
+    }
+    else {
+        console.log('Error');
+    }
+};
+
+exports.update_board = function(game_number, board_string) {
+    if(games_in_progress[game_number]) {
+        games_in_progress[game_number][2] = board_string;
+    }
+    else {
+        console.log('Incorrect game number');
+    }
+};
+
+exports.get_current_games = function() {
+    return games_in_progress;
 };
--- main.js
+++ main.js
@@ -9,3 +9,13 @@ console.log(c_database.get_user_data());
 c_database.remove_user_data('Mom');
 
 console.log(c_database.get_user_data());
+
+c_database.start_new_game('Glenn', 'Lyman');
+c_database.start_new_game('Glenn', 'Lily');
+c_database.start_new_game('Glenn', 'Mom');
+
+console.log(c_database.get_current_games());
+
+c_database.update_board(1, 'r:');
+
+console.log(c_database.get_current_games());
