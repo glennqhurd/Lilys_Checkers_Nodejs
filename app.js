2200d9d6f5b1cac84bcccc9ea1c9e4e27b503d1d
Glenn Hurd <glennqhurd@gmail.com>
2017-10-23 04:50:02 -0400

    Changed code so the javascript and css load properly.  Then added the checkers images to populate the board instead of filler text.

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 //app.use('/users', users);
-//app.use('/', express.static('./public/index.html'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
