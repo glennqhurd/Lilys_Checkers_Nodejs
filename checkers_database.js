4faf1c30788dea6b58346a0274a47995b1f3db11
Glenn Hurd <glennqhurd@gmail.com>
2017-11-14 02:22:02 +0000

    Made further changes to clean up the SQL database functions and removed old unnecessary code.

--- checkers_database.js
+++ checkers_database.js
@@ -75,12 +75,12 @@ function display_moves() {
 
 function update_games_board_by_pk(primary_key, new_string) {
     console.log("UPDATE games SET board_string = " + new_string + " WHERE game_id = " + primary_key);
-    db.run("UPDATE games SET board_string = " + new_string + " WHERE game_id = " + primary_key);
+    db.run("UPDATE games SET board_string = ? WHERE game_id = ?", [new_string, primary_key]);
 }
 
 function update_games_users(primary_key, new_user1, new_user2) {
     console.log("UPDATE games SET user_id1 = " + new_user1 + ", user_id2 = " + new_user2 + " WHERE game_id = " + primary_key);
-    db.run("UPDATE games SET user_id1 = " + new_user1 + ", user_id2 = " + new_user2 + " WHERE game_id = " + primary_key);
+    db.run("UPDATE games SET user_id1 = ?, user_id2 = ? WHERE game_id = ?", [new_user1, new_user2, primary_key]);
 }
 
 function add_new_user(username, email, passwd) {
@@ -115,54 +115,4 @@ db.close((err) => {
     }
     console.log('Close the database connection.');
 });
-
-let user_data = ['Glenn', 'Lyman', 'Lily', 'Delaney'];
-let game_numbers = [];
-let current_game_number = 1;
-let games_in_progress = {};
-
-exports.get_user_data = function() {
-    return user_data;
-};
-
-exports.add_user_data = function(new_name) {
-    if(!user_data.includes(new_name)) {
-        user_data.push(new_name);
-    }
-};
-
-exports.remove_user_data = function(removed_name) {
-    if(user_data.includes(removed_name)) {
-        var index = user_data.indexOf(removed_name);
-        user_data.splice(index, 1);
-    }
-    else {
-        console.log('Name is not in array');
-    }
-};
-
-exports.start_new_game = function(player1, player2) {
-    if((player1 !== player2) && user_data.includes(player1) && user_data.includes(player2)) {
-        game_numbers.push(current_game_number);
-        games_in_progress[current_game_number] = [player1, player2, 'b:bbbbbbbbbbbb--------rrrrrrrrrrrr'];
-        current_game_number++;
-    }
-    else {
-        console.log('Error');
-    }
-};
-
-exports.update_board = function(game_number, board_string) {
-    if(games_in_progress[game_number]) {
-        games_in_progress[game_number][2] = board_string;
-    }
-    else {
-        console.log('Incorrect game number');
-    }
-};
-
-exports.get_current_games = function() {
-    return games_in_progress;
-};
-
 
